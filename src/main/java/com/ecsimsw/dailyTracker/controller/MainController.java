4fe1fb3f1760b90083d852768080926cd157c574
ecsimsw <ecsimsw@gmail.com>
2021-01-23 14:38:48 +0900

    Initial commit

---
+++ src/main/java/com/ecsimsw/dailyTracker/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.ecsimsw.dailyTracker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "dailyTracker";
+    }
+}
