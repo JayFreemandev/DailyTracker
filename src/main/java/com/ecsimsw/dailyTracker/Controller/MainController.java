9f3dc350c703d4d8596d1182a9396cbe7019d657
ecsimsw <ecsimsw@gmail.com>
2021-01-25 22:07:20 +0900

    TODO 리스트 정리 1. 상태 코드를 반환하도록 2. 적절한 http 메소드, url을 사용할 수 있도록

--- src/main/java/com/ecsimsw/dailyTracker/Controller/MainController.java
+++ src/main/java/com/ecsimsw/dailyTracker/Controller/MainController.java
@@ -3,6 +3,10 @@ package com.ecsimsw.dailyTracker.Controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+// TODO :: 상태 코드 반환, @ResponseEntity 공부
+
+// TODO :: 적절한 Http 메소드, URL 컨벤션 공부
+
 @RestController
 @RequiredArgsConstructor
 public class MainController {
